23f938caae78dddef3a4228558609472cdee8712
Ben Humphris <bhumphris@comcast.net>
2016-09-15 15:10:52 -0500

    Add files via upload

---
+++ madlibs2.py
@@ -0,0 +1,69 @@
+print("This is my version of madlibs")
+print("hopefully you will enjoy this...")
+print("type a person")
+person = raw_input()
+if person == "":
+    person = "Donald Trump"
+print("type a verb")
+verb = raw_input()
+if verb == "":
+    verb = "laugh"
+print("type an adjective")
+adjective = raw_input()
+if adjective == "":
+    adjective = "ugly"
+print("type verb two")
+verb_two = raw_input()
+if verb_two == "":
+    verb = "cry"
+print("type a noun")
+noun = raw_input()
+if noun == "":
+    noun = "tv"
+print("type noun two")
+noun_two = raw_input()
+if noun_two == "":
+    noun_two = "truck"
+print("type adjective two")
+adjective_two = raw_input()
+if adjective_two == "":
+    (adjective_two) = "smart"
+print("type person two")
+person_two = raw_input()
+if person_two == "":
+    person_two = "Bernie Sander's"
+print("type verb two")
+verb_two = raw_input()
+if verb_two == "":
+    verb_two = "jumped"
+print("type adjective three")
+adjective_three = raw_input()
+if adjective_three == "":
+    adjective_three = "curious"
+print("type verb three")
+verb_three = raw_input()
+if verb_three == "":
+    verb_three = "bounced"
+print("type noun three")
+noun_three = raw_input()
+if noun_three == "":
+    noun_three = "boats"
+#poem
+print("Three" + adjective + person)
+print("Three" + adjective_2 + person)
+
+print("See" + "how" + "they" + verb)
+print("See" + "how" + "they" + verb_2)
+
+print("They" "all" + "ran" + "after" + "the" "person_2" + noun)
+print("Who" + verb_3 + "off" + "their" + noun_2)
+print("With" +"a" +"carving" +"knife")
+
+print("Did" + "you" + "ever" + "see" + "such" + "a" + "thing" + "in" +"your" + noun_2)
+print("As" + "three" + adjective_3 + noun_3)
+
+
+
+
+
+
